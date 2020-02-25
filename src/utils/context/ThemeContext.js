0d1b0de8f4bcc44307338243c8a3dd4e7747038a
Jacob D. Castro <jdcastro.business@gmail.com>
2020-02-24 23:20:09 -0800

    light/dark theme changes based on time of day

--- src/utils/context/ThemeContext.js
+++ src/utils/context/ThemeContext.js
@@ -1,51 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { lightTheme, darkTheme } from '../../styles/layout/__themes';
 import { ThemeContext } from './index';
 
+const setDefaultTheme = () => {
+  const today = new Date();
+  const hour = today.getHours();
+
+  if (hour >= 19 || hour <= 6) return darkTheme;
+  else if (hour > 6 && hour < 19) return lightTheme;
+  else return lightTheme;
+};
+
 // ? provider component wraps entire app in gatsby-browser.js
-// class ThemeContextWrapper extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     // TODO set initial theme based on time of day
-//     // ? bug in header background color on scroll when code isn't commented
-//     // this.setTheme = () => {
-//     //   const now = new Date();
-//     //   const time = now.getHours();
-
-//     //   if (time >= 19 || time <= 6) {
-//     //     return darkTheme;
-//     //   } else if (time > 6 && time < 19) {
-//     //     return lightTheme;
-//     //   }
-//     // };
-
-//     this.toggleTheme = () => {
-//       this.setState({
-//         currentTheme:
-//           this.state.currentTheme === lightTheme ? darkTheme : lightTheme,
-//       });
-//     };
-
-//     this.state = {
-//       currentTheme: lightTheme,
-//       transition: '0.5s',
-//       toggleTheme: this.toggleTheme,
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <ThemeContext.Provider value={this.state}>
-//         {this.props.children}
-//       </ThemeContext.Provider>
-//     );
-//   }
-// }
-
 const ThemeContextWrapper = ({ children }) => {
   const [themeState, setThemeState] = useState({
-    currentTheme: lightTheme,
+    currentTheme: setDefaultTheme(),
     transition: '0.5s',
   });
 
@@ -58,23 +27,6 @@ const ThemeContextWrapper = ({ children }) => {
   };
 
   if (!themeState.toggleTheme) setThemeState({ ...themeState, toggleTheme });
-
-  useEffect(() => {
-    const now = new Date();
-    const time = now.getHours();
-
-    if (time >= 19 || time <= 6) {
-      setThemeState({
-        ...themeState,
-        currentTheme: darkTheme,
-      });
-    } else if (time > 6 && time < 19) {
-      setThemeState({
-        ...themeState,
-        currentTheme: lightTheme,
-      });
-    }
-  }, []);
 
   return (
     <ThemeContext.Provider value={themeState}>{children}</ThemeContext.Provider>
--- src/utils/context/index.js
+++ src/utils/context/index.js
@@ -1,8 +1,17 @@
 import { createContext } from 'react';
-import { lightTheme } from '../../styles/layout/__themes';
+import { lightTheme, darkTheme } from '../../styles/layout/__themes';
+
+const setDefaultTheme = () => {
+  const today = new Date();
+  const hour = today.getHours();
+
+  if (hour >= 19 || hour <= 6) return darkTheme;
+  else if (hour > 6 && hour < 19) return lightTheme;
+  else return lightTheme;
+};
 
 export const ThemeContext = createContext({
-  currentTheme: lightTheme,
+  currentTheme: setDefaultTheme(),
   transition: '0.5s',
   toggleTheme: () => {},
 });
